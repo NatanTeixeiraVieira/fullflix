e798a70a0f8094bd8a1fd7f86d4797ee38b99a23
NatanTeixeiraVieira <natanteixeiravieira@gmail.com>
2023-04-23 17:09:21 -0300

    style: Add hamburger menu in header on mobile devices

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,6 @@
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { useContext, useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import SearchIcon from '@mui/icons-material/Search';
 import './styles.css';
 import ClearIcon from '@mui/icons-material/Clear';
@@ -11,7 +11,9 @@ export default function Header() {
   const [blackHeader, setBlackHeader] = useState(false);
   const [hideHeaderPart, setHideHeaderPart] = useState(false);
   const [searchInput, setSearchInput] = useState('');
+  const [showMenu, setShowMenu] = useState(false);
   const navigate = useNavigate();
+  const { pathname } = useLocation();
 
   useEffect(() => {
     const scrollMove = () => {
@@ -54,12 +56,19 @@ export default function Header() {
     setSearchInput('');
   };
 
+  const handleShowMenu = () => {
+    setShowMenu((prev) => !prev);
+  };
+
   return (
-    <header className={blackHeader ? 'fillBackground' : ''}>
+    <header className={blackHeader && 'fillBackground'}>
       <div className="site_name">
         <Link to="/">Fullflix</Link>
       </div>
-      <div className={`content_not_in_login ${hideHeaderPart ? 'hidden' : ''}`}>
+      <div
+        className={`content_not_in_login ${hideHeaderPart && 'hidden'}`}
+        style={{ right: showMenu ? '0' : '-300px' }}
+      >
         <nav className="navigation_menu">
           <ul>
             <li>
@@ -71,32 +80,38 @@ export default function Header() {
           </ul>
         </nav>
         <div className="header_right_side">
-          <div className="search_area">
-            <div className="search_button" onClick={handleSearch}>
-              <SearchIcon />
-            </div>
-            <div className="search_input_and_delete_button">
-              <input
-                type="text"
-                className="search_input"
-                placeholder="Pesquisar"
-                value={searchInput}
-                onChange={handleSearchInput}
-                onKeyDown={handleSearch}
-              />
-              <div className="clear_icon" onClick={handleClickClearIcon}>
-                {searchInput.length > 0 && (
-                  <ClearIcon
-                    style={{
-                      fontSize: '1.2rem',
-                      color: '#444',
-                      cursor: 'default',
-                    }}
-                  />
-                )}
+          {pathname === '/' && (
+            <div
+              className="search_area"
+              style={{ display: showMenu && 'none' }}
+            >
+              <div className="search_button" onClick={handleSearch}>
+                <SearchIcon />
+              </div>
+              <div className="search_input_and_delete_button">
+                <input
+                  type="text"
+                  className="search_input"
+                  placeholder="Pesquisar"
+                  value={searchInput}
+                  onChange={handleSearchInput}
+                  onKeyDown={handleSearch}
+                />
+                <div className="clear_icon" onClick={handleClickClearIcon}>
+                  {searchInput.length > 0 && (
+                    <ClearIcon
+                      style={{
+                        fontSize: '1.2rem',
+                        color: '#444',
+                        cursor: 'default',
+                      }}
+                    />
+                  )}
+                </div>
               </div>
             </div>
-          </div>
+          )}
+
           <Link to="/account">
             <AccountCircleIcon
               style={{
@@ -108,6 +123,14 @@ export default function Header() {
               }}
             />
           </Link>
+          <div
+            className={`menu_lines ${showMenu && 'menu_lines_animation'}`}
+            onClick={handleShowMenu}
+          >
+            <span />
+            <span />
+            <span />
+          </div>
         </div>
       </div>
     </header>
